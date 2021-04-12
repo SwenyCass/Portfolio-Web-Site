8c6dd86d8d4f773c80904854a9c8640a5be397c9
SwenyCassinoni <swenycassinoni@hotmail.com>
2021-04-12 00:32:34 -0300

    Desenvolvimento Web Site Portfolio

---
+++ Portfolio.WebSite/Portfolio.WebSite/Controllers/CertificadosController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Portfolio.WebSite.Controllers
+{
+    public class CertificadosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Portfolio.WebSite/Portfolio.WebSite/Controllers/ExperienciasController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Portfolio.WebSite.Controllers
+{
+    public class ExperienciasController : Controller
+    {
+        //[Route("Experiencias/Index")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
